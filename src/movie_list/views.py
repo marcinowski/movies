b0b91e037e74726e251165cf848181e674e5bc2d
Marcin <muszynskimarcin@wp.pl>
2016-11-06 00:09:14 +0100

    User templates modifications

--- src/movie_list/views.py
+++ src/movie_list/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import resolve_url, HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
 from movie_list.models import Movie, Genre
 from omdb.service import OMDBFetcher
+from omdb.fetcher import MovieNotFound
 from movie_list.services import MovieService
 from movie_list.mapping import LIST_VIEW_MAPPING
 
@@ -53,10 +54,14 @@ class MovieCreateView(CreateView):
         form = request.GET.get('form', '')
         context = self.get_context_data()
         if (title and year) or imdb_id:
-            selected_movie = OMDBFetcher().get(title=title, year=year, imdb_id=imdb_id)
-            context['result'] = selected_movie
-            if form:
-                context['movie'] = selected_movie[0]
+            try:
+                selected_movie = OMDBFetcher().get(title=title, year=year, imdb_id=imdb_id)
+            except MovieNotFound:
+                context['alert'] = 'No movie was found for given parameters. Check the data or try filling form.'
+            else:
+                context['result'] = selected_movie
+                if form:
+                    context['movie'] = selected_movie[0]
         elif title:
             context['result'] = OMDBFetcher().page_search(title=title, page=1)
         else:
--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-
+from django.contrib.auth import urls
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
