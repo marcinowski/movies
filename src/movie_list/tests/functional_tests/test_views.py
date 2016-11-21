f89b0bdf7fc07de7a799afb417ee67f4f628cec2
Marcin <muszynskimarcin@wp.pl>
2016-11-21 02:21:28 +0100

    Further movie_list.views tests

--- src/movie_list/tests/functional_tests/test_views.py
+++ src/movie_list/tests/functional_tests/test_views.py
@@ -1,16 +1,33 @@
+from urllib.parse import urlencode
+from collections import OrderedDict
 from django.test import TestCase
 from django.shortcuts import resolve_url
 from django.contrib.auth.models import User
-from unittest import skip
+from unittest.mock import patch
 from movie_list.models import Movie, Genre, Country, Person
 
 
-class TestCollectionView(TestCase):
+class TestMoviesView(TestCase):
     def setUp(self):
-        self.url = resolve_url('/movie_list/')
         self.user = User.objects.create_user(username='test_user', password='test_pwd')
         self.user_2 = User.objects.create_user(username='test_user_2', password='test_pwd')
         self.logged = self.client.login(username='test_user', password='test_pwd')
+        self.url = '/'
+
+    def test_user_is_authenticated(self):
+        self.assertTrue(self.user.is_active)
+        self.assertTrue(self.user.is_authenticated())
+        self.assertTrue(self.logged)
+
+    def test_response_200(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestCollectionView(TestMoviesView):
+    def setUp(self):
+        super(TestCollectionView, self).setUp()
+        self.url = resolve_url('/movie_list/')
         self.person_1 = Person.objects.create(name='test_person_1')
         self.person_2 = Person.objects.create(name='test_person_2')
         self.genre_1 = Genre.objects.create(name='comedy')
@@ -27,11 +44,6 @@ class TestCollectionView(TestCase):
         self.movie_1.actors.add(self.person_1)
         self.movie_1.director.add(self.person_2)
         self.movie_1.genre.add(self.genre_1)
-
-    def test_user_is_authenticated(self):
-        self.assertTrue(self.user.is_active)
-        self.assertTrue(self.user.is_authenticated())
-        self.assertTrue(self.logged)
 
     def test_response_200(self):
         response = self.client.get(self.url)
@@ -53,3 +65,86 @@ class TestCollectionView(TestCase):
     def test_filtering_by_genre(self):
         response = self.client.get(self.url + '?genre=comedy')
         self.assertEqual(len(response.context['object_list']), 1)
+
+
+class TestMovieDetailView(TestMoviesView):
+    def setUp(self):
+        super(TestMovieDetailView, self).setUp()
+        self.movie_1 = Movie.objects.create(title='movie_1', year=2000, user=self.user)
+        self.movie_2 = Movie.objects.create(title='movie_2', year=2000, user=self.user_2)
+        self.url = resolve_url('/movie_list/{}/'.format(self.movie_1.id))
+
+    def test_response_404_for_movie_out_of_user_db(self):
+        url = resolve_url('/movie_list/{}/'.format(self.movie_2.id))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_response_context(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.context['movie'], self.movie_1)
+
+
+class TestMovieCreateView(TestMoviesView):
+    def setUp(self):
+        super(TestMovieCreateView, self).setUp()
+        self.url = resolve_url('/movie_list/add/')
+
+    @patch('movie_list.views.OMDBFetcher.page_search', return_value={'title': 'test'})
+    def test_search_title(self, mock):
+        response = self.client.get(self.url + '?title=test')
+        self.assertEqual(response.context['result'], {'title': 'test'})
+
+    @patch('movie_list.views.OMDBFetcher.get', return_value=[{'title': 'test', 'year': 2000}])
+    def test_get_title_and_year(self, mock):
+        response = self.client.get(self.url + '?title=test&year=2000')
+        self.assertEqual(response.context['result'], [{'title': 'test', 'year': 2000}])
+        response = self.client.get(self.url + '?title=test&year=2000&form=true')
+        self.assertEqual(response.context['result'], [{'title': 'test', 'year': 2000}])
+        self.assertEqual(response.context['movie'], {'title': 'test', 'year': 2000})
+
+    @patch('movie_list.views.OMDBFetcher.get', return_value=[{'title': 'test', 'year': 2000}])
+    def test_get_imdb_id(self, mock):
+        response = self.client.get(self.url + '?imdb_id=1')
+        self.assertEqual(response.context['result'], [{'title': 'test', 'year': 2000}])
+        response = self.client.get(self.url + '?imdb_id=1&form=true')
+        self.assertEqual(response.context['result'], [{'title': 'test', 'year': 2000}])
+        self.assertEqual(response.context['movie'], {'title': 'test', 'year': 2000})
+
+    def test_post(self):
+        data = {
+            'title': 'test_movie',
+            'year': 1999,
+            'actors': 'actor_test, test_actor',
+            'director': 'test_director',
+            'genre': 'test_genre, test_genre_2',
+            'country': 'test_country',
+            'description': 'test_description'
+        }
+        response = self.client.post(self.url, data=data)
+        self.assertRedirects(response, resolve_url('/movie_list/'))
+        movie = Movie.objects.get(title='test_movie')
+        self.assertIsInstance(movie, Movie)
+        self.assertEqual(Person.objects.all().count(), 3)
+        self.assertEqual(Genre.objects.all().count(), 2)
+        self.assertIsInstance(Country.objects.get(name='test_country'), Country)
+        self.assertEqual([movie.title, movie.year, movie.description], ['test_movie', 1999, 'test_description'])
+        self.assertEqual(movie.user, self.user)
+
+
+class TestFetchOMDBDataView(TestMoviesView):
+    def setUp(self):
+        super(TestFetchOMDBDataView, self).setUp()
+        self.url = resolve_url('/movie_list/fetch/')
+
+    def test_response_200(self):
+        pass
+
+    @patch('movie_list.views.OMDBFetcher.get')
+    def test_post(self, mock):
+        data = OrderedDict(title='test', year=2000, imdb_id=1)
+        response = self.client.post(self.url, data=data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue('/movie_list/add/' in response.url)
+        self.assertTrue('title=test' in response.url)
+        self.assertTrue('year=2000' in response.url)
+        self.assertTrue('imdb_id=1' in response.url)
