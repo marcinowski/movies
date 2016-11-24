4bcf93c3bbb2ec00b382a2a4a35bc1501e998791
Marcin <muszynskimarcin@wp.pl>
2016-11-24 01:01:25 +0100

    Deploy more info

--- src/movies/settings/production.py
+++ src/movies/settings/production.py
@@ -6,3 +6,9 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ALLOWED_HOSTS.append('https://collectmovies.herokuapp.com/')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
